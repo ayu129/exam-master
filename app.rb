19ad03edb9d4840cb469a660e43f71f35dee5767
ayu129 <ayu.daddybear.29@gmail.com>
2019-04-14 07:53:17 +0000

    first commit

--- app.rb
+++ app.rb
@@ -21,9 +21,29 @@ redirect ‘/‘
 end
 
 # 特定のメモを編集するときの処理
-
+post ‘edit/:id’ do
+ @item = Item.find(params[:id])
+ erb :edit
+end
 # 特定のメモを更新するときの処理
-
+post ‘renew/:id’ do
+ @item = Item.find(params[:id])
+ @item.update({
+  title: params[:title],
+  body: params[:body]
+ })
+redirect ‘/‘
+end
 # 特定のメモを削除するときの処理
-
+post ‘/delete/:id’ do
+ Item.find(params[:id]).destroy
+ redirect ‘/‘
+end
 # メモをカテゴリ分類して表示するときの処理
+get ‘/category/:id’ do
+ @categories = Category.all
+ @cagtegory = Category.find(params[:id])
+ @category_name = @category.name
+ @items = @category.items
+erb :index
+end
